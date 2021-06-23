c629a8ab6fa176d909ba41705131785a94cd9302
kushal <kushal@gmx.com>
2021-06-23 17:49:44 -0600

    initialize git repo

---
+++ src/server/TeamsAppsComponents.ts
@@ -0,0 +1,4 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the babyTalkTab tab
+export * from "./babyTalkTab/BabyTalkTab";
---
+++ src/server/babyTalkTab/BabyTalkTab.ts
@@ -0,0 +1,8 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/babyTalkTab/index.html")
+export class BabyTalkTab {
+}
